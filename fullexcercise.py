804ac974c463e446747990693e0e363986709261
NikolaAnovic <anovicnikola33@gmail.com>
2021-12-08 19:08:01 +0100

    Rename files

---
+++ fullexcercise.py
@@ -0,0 +1,25 @@
+class Vehicle:
+    color = "White"
+    def __init__(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+    def seats_method(self,seating_capacity):
+        self.seating_capacity = seating_capacity
+        return f'This vehicle has a seating capacity of {seating_capacity}'
+    def fare_method(self):
+        return self.seating_capacity * 100
+
+class Bus(Vehicle):
+    def __init__(self,max_speed,mileage):
+        super().__init__(max_speed,mileage)
+    def seats_method(self,seating_capacity=50):
+        return super().seats_method(seating_capacity=50)
+    def fare_method(self):
+        amount = super().fare_method()
+        amount += amount * 10 /100
+        return int(amount)
+
+mercedes = Bus(30,3000)
+audi = Vehicle(300,75000)
+print(mercedes.seats_method(),"and a fare of this vehicle is",mercedes.fare_method())
+print(audi.seats_method("5"))
